d1145222bd5bad0db2e2095e2307a6f0aa2955d5
Lutfur Rahaman <lutfur.rahaman@tcs.com>
2019-04-05 13:13:15 +0530

    US70 : InterestController added with dummy content

---
+++ com/piggybank/src/app/controller/InterestRuleController.java
@@ -0,0 +1,21 @@
+package com.piggybank.src.app.controller;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+public class InterestRuleController {
+ 
+    @RequestMapping(value="/hello-page")
+    public ModelAndView goToHelloPage() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("hello"); //name of the jsp-file in the "page" folder
+         
+        String str = "MVC Spring is here!";
+        view.addObject("message", str); //adding of str object as "message" parameter
+         
+        return view;
+    }
+     
+}
